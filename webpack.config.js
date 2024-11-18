be18b605a4e872030957cb2932a89df7e0d6212d
K-sesha <20103952@tafe.wa.edu.au>
2024-11-18 17:34:39 +0800

    different webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,35 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/", // Ensure publicPath is set correctly for SPA routing
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|gif|jpe?g|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[path][name].[ext]",
-              context: path.resolve(__dirname, "dist"), // keeps folder structure
-              outputPath: "assets/", // output location
-              publicPath: "assets/", // path for your assets in CSS
+              name: '[name].[hash].[ext]',
+              outputPath: 'images',
             },
           },
         ],
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true, // Add this line to handle SPA routing
   },
 };
